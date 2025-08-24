aa6876a35cb5f370fd0a1dcc54873d55c4c5d88a
Simen Bihaug-Frøyland <sbfroyland@gmail.com>
2025-08-24 11:30:49 +0000

    Some refactoring to improve config structure

--- src/config.py
+++ src/config.py
@@ -1,8 +1,12 @@
-CONFIG = {
+config = {
     "scope": {
-        "pdfs": [
-            "data/test_data/reguleringsbestemmelser.pdf"
-        ]
+        "sentence_splitter_model": "nb_core_news_md",
+        "target_token_len": 2000,
+        "overlap_token_len": 250,
+        "test_scope_01": {
+            "scope_folder": "data/test_scopes/scope_01",
+            "supported_types": ["pdf", "url"]
+        }
     },
     "generate": {},
     "cluster": {},
