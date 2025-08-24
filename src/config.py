72d6603fc39134f7d2df3129c33164a8a30afc8d
Simen Bihaug-Frøyland <sbfroyland@gmail.com>
2025-08-24 14:06:37 +0000

    Removed overlap functionality

--- src/config.py
+++ src/config.py
@@ -2,7 +2,6 @@ config = {
     "scope": {
         "sentence_splitter_model": "nb_core_news_md",
         "target_token_len": 2000,
-        "overlap_token_len": 250,
         "test_scope_01": {
             "scope_folder": "data/test_scopes/scope_01",
             "supported_types": ["pdf", "url"]
