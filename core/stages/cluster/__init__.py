7a169d00fe2bb11a6dce4597d43fb6a629a6ebb3
Simen Bihaug-Frøyland <sbfroyland@gmail.com>
2025-09-11 18:54:09 +0000

    Remove unused imports from cluster module

--- core/stages/cluster/__init__.py
+++ core/stages/cluster/__init__.py
@@ -1,13 +1,5 @@
 from .main import run_cluster_stage
-from .config import ClusterConfig
-from .clusterer import HDBSCANClusterer
-from .embedding_generator import EmbeddingGenerator
-from .dimensionality_reducer import DimensionalityReducer
 
 __all__ = [
     "run_cluster_stage",
-    "ClusterConfig",
-    "HDBSCANClusterer",
-    "EmbeddingGenerator", 
-    "DimensionalityReducer"
 ]
