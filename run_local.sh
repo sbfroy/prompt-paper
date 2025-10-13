f8974f1f02f6a8c0e9495c23022e3b9fc0767810
Simen Bihaug-Frøyland <sbfroyland@gmail.com>
2025-10-13 18:14:23 +0000

    Removed unused file

--- run_local.sh
+++
@@ -1,9 +0,0 @@
-set -euo pipefail # Exit on error (safety setting)
-IMAGE=prompt-paper:latest
-
-docker run --rm -it \
-  --add-host=host.docker.internal:host-gateway \
-  --env-file .env \
-  -v "$PWD":/workspace -w /workspace \
-  "$IMAGE" \
-  bash
