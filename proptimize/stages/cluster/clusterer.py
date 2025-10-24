5ef337c8fedf823ddbf6f5e0a707c574e2951541
Simen Bihaug-Frøyland <sbfroyland@gmail.com>
2025-10-24 10:53:28 +0000

    Removed unnecessary config.py and implemented better param interaction between yaml and code

--- proptimize/stages/cluster/clusterer.py
+++ proptimize/stages/cluster/clusterer.py
@@ -2,21 +2,8 @@ import hdbscan
 
 
 class HDBSCANClusterer:
-    def __init__(
-        self, min_cluster_size, min_samples, cluster_selection_epsilon, **kwargs
-    ):
-        self.clusterer = hdbscan.HDBSCAN(
-            # min_cluster_size=min_cluster_size,
-            # min_samples=min_samples,
-            # cluster_selection_epsilon=cluster_selection_epsilon,
-            min_cluster_size=10,
-            min_samples=10,
-            cluster_selection_epsilon=0.30,
-            cluster_selection_persistence=0.7,
-            max_cluster_size=200,
-            cluster_selection_method="eom",
-            **kwargs
-        )
+    def __init__(self, **kwargs):
+        self.clusterer = hdbscan.HDBSCAN(**kwargs)
 
     def cluster(self, embeddings):
         """
