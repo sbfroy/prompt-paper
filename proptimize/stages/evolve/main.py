940a79b1cc21b826fc841f55f9370971f671cab7
Simen Bihaug-Frøyland <sbfroyland@gmail.com>
2025-10-26 11:51:13 +0000

    Fixed absolute imports

--- proptimize/stages/evolve/main.py
+++ proptimize/stages/evolve/main.py
@@ -3,9 +3,9 @@ from deap import tools
 
 logging.basicConfig(level=logging.INFO)
 
-from .experiment import GA
-from .operators import mate, composite_mutate
-from ...data_manager import DataManager
+from proptimize.data_manager import DataManager
+from proptimize.stages.evolve.experiment import GA
+from proptimize.stages.evolve.operators import mate, composite_mutate
 
 class EvolveStage:
     def __init__(self, data_manager, config, eval_fn):
